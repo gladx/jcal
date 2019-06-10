b8e4ac8f2587eb0d87aebb5920b2cec4bc22e6c3
Ali Yousefi <Aliyousefi.tec@gmail.com>
2019-06-10 21:12:48 +0430

    today format with link

--- src/Jcal.php
+++ src/Jcal.php
@@ -10,24 +10,23 @@ class Jcal
         $today = new Jalali(null, 'Asia/Tehran');
         $startOfMonth = (new Jalali(null, 'Asia/Tehran'))->startOfMonth();
         $endOfMonth = (new Jalali(null, 'Asia/Tehran'))->endOfMonth();
-        $str = 'l ' . $today->format('y l d F') . " l\n";
-        $str .= 'Sh Ye Do Se Ch Pa Jo' . "\n";
+        $str = 'l ' . $today-> format('y l d F') . " l\n";
+        $str .= ' Sh  Ye   Do  Se  Ch  Pa  Jo'."\n";
         $startFirstDayinWeek = $startOfMonth->format('N');
         for ($i = 0; $i <= $startFirstDayinWeek; $i++) {
-            $str .= '     ';
+            $str .= '       ';
         }
 
         for ($currentDay = clone $startOfMonth; $currentDay <= $endOfMonth; $currentDay->addDays(1)) {
-            if ($currentDay->day == $today->day) {
-                $num = "*" . $currentDay->format('d') . "*";
-                $str .=  sprintf("%-5s", $num);
+            if($currentDay->day == $today->day) {
+                $str .= " ". "[".$currentDay->format('d')."](http://t.me/jcal2bot/)". "  ";
             } else {
-                $str .= sprintf("%-3s", $currentDay->format('d'));
+                $str .= "`".sprintf("%-3s",$currentDay->format('d')) . "`";
             }
 
-            if ((($currentDay->format('N')) % 5) == 0) {
+            if((($currentDay->format('N')) % 5) == 0) {
                 $str .= "\n";
-            }
+            } 
         }
         return $str;
     }
